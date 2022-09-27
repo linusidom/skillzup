0402ec89277c31d78a2a2cbd2ad177535c5f8762
linusidom <linusidom@gmail.com>
2022-09-27 18:24:08 +0700

    first commit

--- src/components/SelfPaced/SelfPacedMachineLearning.js
+++ src/components/SelfPaced/SelfPacedMachineLearning.js
@@ -4,19 +4,23 @@ export const SelfPacedMachineLearning = () => {
     return(
         <div className='ml row p-5'>
             <div className="mlImage col">
-                <img className='fluid' src={ml} alt="sometext" />
+                <a className='link' href='https://www.youtube.com/watch?v=-rcDUSeQw_M&list=PLA_Vc1i8J5NIhUNds8S7bA2c_HsJbQ7gh' target='_blank' rel="noreferrer">
+                    <img className='fluid' src={ml} alt="sometext" />
+                </a>
             </div>
 
             <div className="mlText col">
                 <div className="redLine"></div>
-                <p className="mlHeader headerText">Data Sciences with Python</p>
-                <ul>
-                    <li>Learn how todays leading data scientists predict data</li>
+                <a className='link' href='https://www.youtube.com/watch?v=-rcDUSeQw_M&list=PLA_Vc1i8J5NIhUNds8S7bA2c_HsJbQ7gh' target='_blank' rel="noreferrer">
+                    <p className="mlHeader headerText">Data Sciences with Python</p>
+                    <ul>
+                        <li>Learn how todays leading data scientists predict data</li>
 
-                    <li>Understand how to use the leading libraries such as SciKit Learn and TensorFlow</li>
+                        <li>Understand how to use the leading libraries such as SciKit Learn and TensorFlow</li>
 
-                    <li>Learn how to intergrate with the existing community of Machine Learning Developers</li>
-                </ul>
+                        <li>Learn how to intergrate with the existing community of Machine Learning Developers</li>
+                    </ul>
+                </a>
             </div>
         </div>
 
--- src/components/SelfPaced/SelfPacedPython.js
+++ src/components/SelfPaced/SelfPacedPython.js
@@ -5,15 +5,19 @@ export const SelfPacedPython = () => {
             <div className='python row p-5'>
                 <div className="pythonText col">
                     <div className="redLine"></div>
-                    <p className="pythonHeader headerText">Python<br/>Full Stack Developer</p>
-                    <ul>
-                        <li>HTML, CSS and JavaScript for the Frontend</li>
-                        <li>Learn the Django Framework</li>
-                        <li>Create API's and manage data with SQL</li>
-                    </ul>
+                    <a className='link' href='https://www.youtube.com/watch?v=_cDm9_-mj-s&list=PLA_Vc1i8J5NL7JvqvZs6CtXvJ19EmnNzv' target='_blank' rel="noreferrer">
+                        <p className="pythonHeader headerText">Python<br/>Full Stack Developer</p>
+                        <ul>
+                            <li>HTML, CSS and JavaScript for the Frontend</li>
+                            <li>Learn the Django Framework</li>
+                            <li>Create API's and manage data with SQL</li>
+                        </ul>
+                    </a>
                 </div>
                 <div className="pythonImage col">
-                    <img className='fluid' src={python} alt="sometext" />
+                    <a className='link' href='https://www.youtube.com/watch?v=_cDm9_-mj-s&list=PLA_Vc1i8J5NL7JvqvZs6CtXvJ19EmnNzv' target='_blank' rel="noreferrer">
+                        <img className='fluid' src={python} alt="sometext" />
+                    </a>
                 </div>
             </div>
     )
